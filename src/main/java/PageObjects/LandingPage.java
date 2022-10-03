a71301670996658bf1f5767a07103bc2f00a89a5
Sayali <sayzshelke@gmail.com>
2022-10-03 13:51:04 +0530

    commited

---
+++ src/main/java/PageObjects/LandingPage.java
@@ -0,0 +1,15 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class LandingPage {
+	
+	
+	WebDriver driver;
+	public LandingPage(WebDriver driver)
+	{
+		//initialization code
+		this.driver = driver;
+	}
+
+}
